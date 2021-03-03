ade2d3212bec8ec208f5152d54c64b5bbdccef13
Houcem Hedhly <hedhoucem@gmail.com>
2021-03-03 14:18:10 +0100

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+  <router-link class="navbar-brand" to="/">TODO-APP </router-link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <router-link class="nav-link" to="/">Home</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/about">About</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/contact">Contact</router-link>
+      </li>
+    </ul>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style>
+
+</style>
