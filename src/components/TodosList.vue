c4cb75fc7eb799c6bae098a43607fccb0bfc8257
Houcem Hedhly <hedhoucem@gmail.com>
2021-03-04 10:23:47 +0100

    toggle completed

--- src/components/TodosList.vue
+++ src/components/TodosList.vue
@@ -2,7 +2,21 @@
     <div>
         <div class="list-group">
             <div v-for="todo in todos" v-bind:key="todo.id" class="list-group-item">
-                <span v-bind:class="{'is-completed': todo.completed}">{{ todo.title }}</span>
+                <input type="checkbox" class="mr-2"
+                    :checked="todo.completed" 
+                    @change="toggleCompleted(todo)">
+                    <!--
+                        shorthand syntax
+                        v-bind:checked <==> :checked
+                        v-on:click <==> @click
+                    -->
+                <span v-bind:class="{'is-completed': todo.completed}">
+                    {{ todo.title }}
+                </span>
+                <button
+                    class="badge badge-danger badge-pill float-right"
+                    >X</button>
+
             </div>
         </div>
     </div>
@@ -13,6 +27,11 @@ export default {
     name: 'TodosList',
     props: {
         todos: Array
+    },
+    methods: {
+        toggleCompleted: function(todo) {
+            todo.completed = !todo.completed;
+        }
     }
 }
 </script>
